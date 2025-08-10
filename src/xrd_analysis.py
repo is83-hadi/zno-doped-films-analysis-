e6887fe3f90880ca156dd4be492be422f0c06e20
Dr.Israa Hadi Hilal <israa.h.hilal@src.edu.iq>
2025-08-10 08:41:28 +0300

    xrd_analysis
    XRD Analysis Tools for ZnO Doped Films
    ======================================
    
    Related Publication:
    Jabbar et al., AIP Conf. Proc. 2414, 030001 (2023)
    DOI: 10.1063/5.0114811
    """
    
    # Main analysis code will be added here
    print("ZnO Analysis Tools - Coming Soon!")
    
    Signed-off-by: Dr.Israa Hadi Hilal  <israa.h.hilal@src.edu.iq>

---
+++ src/xrd_analysis.py
@@ -0,0 +1,10 @@
+XRD Analysis Tools for ZnO Doped Films
+======================================
+
+Related Publication:
+Jabbar et al., AIP Conf. Proc. 2414, 030001 (2023)
+DOI: 10.1063/5.0114811
+"""
+
+# Main analysis code will be added here
+print("ZnO Analysis Tools - Coming Soon!")
